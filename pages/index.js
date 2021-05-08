8997746005364670aaf0323ddcf9b12b1eb8d22c
Damla Köksal <me@damlakoksal.com>
2021-05-08 17:38:14 +0300

    feat: add dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
-import Link from "next/link";
+import { Button, ButtonGroup, Stack, useColorMode } from "@chakra-ui/react"
 
 export default function IndexPage() {
+  const { colorMode, toggleColorMode } = useColorMode()
   return (
-    <div>
-      Hello World.{" "}
-      <Link href="/about">
-        <a>About Emre</a>
-      </Link>
-    </div>
-  );
+    <header>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
+
+      <Stack spacing={2} direction="row" align="center">
+  <Button colorScheme="teal" size="xs">
+    Button
+  </Button>
+  <Button colorScheme="teal" size="sm">
+    Button
+  </Button>
+  <Button colorScheme="teal" size="md">
+    Button
+  </Button>
+  <Button colorScheme="teal" size="lg">
+    Button
+  </Button>
+</Stack>
+    </header>
+  )
 }
