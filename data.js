9f49203ac951d2baaa99be031a5ca8c29fe115cb
Damla Köksal <me@damlakoksal.com>
2021-05-09 04:28:46 +0300

    [#8] feat: basic data modeling done

--- data.js
+++ data.js
@@ -1,12 +1,79 @@
 export const questions = [
     {
       qid: '1',
-      title: 'Question 1',
-      content: 'lorem ipsum falan filan'
+      title: 'Sizce Çarşamba hangi renktir?',
+
     },
     {
       qid: '2',
       title:'Question 2',
       content: 'lorem ipsum falan filan'
     }
+]
+
+export const colors = [
+  {
+    cid: 1,
+    color: 'red',
+    hex: '',
+    letters: [
+      'A', 'M', 'R'
+    ]
+  },
+  {
+    cid: 2,
+    color: 'blue',
+    hex: '',
+    letters: [
+      'B', 'D', 'T', 'W'
+    ]
+  },
+  {
+    cid: 3,
+    color: 'green',
+    hex: '',
+    letters: [
+      'E', 'F', 'G', 'Ğ'
+    ]
+  },
+  {
+    cid: 4,
+    color: 'yellow',
+    hex: '',
+    letters: [
+      'C','Ç', 'L', 'Y'
+    ]
+  },
+  {
+    cid: 5,
+    color: 'orange',
+    hex: '',
+    letters: [
+      'H', 'J', 'K', 'N', 'U', 'Ü'
+    ]
+  },
+  {
+    cid: 6,
+    color: 'purple',
+    hex: '',
+    letters: [
+      'P', 'Q', 'V'
+    ]
+  },
+  {
+    cid: 7,
+    color: 'white',
+    hex: '',
+    letters: [
+      'I','İ', 'O', 'Ö'
+    ]
+  },
+  {
+    cid: 8,
+    color: 'black',
+    hex: '',
+    letters: [
+      'X','Z'
+    ]
+  }
 ]
